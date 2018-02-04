270bbe07b0921023f5e160bc7e7ebaac1a68beae
leon332157 <leon332157@sina.com>
2018-02-04 14:00:50 -0500

    .

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from img2html.converter import Img2HTMLConverter
 import os
 
-converter = Img2HTMLConverter(font_size=1, background='black')
+converter = Img2HTMLConverter(font_size=10, background='black')
 html = converter.convert(os.getcwd() + '/pic.png')
 with open(os.getcwd() + '/a.html', mode='w') as f:
     f.write(str(html))
