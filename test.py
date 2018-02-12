623e8ee237be091d44c32cc210aaea98055e81b7
leon332157 <leon332157@sina.com>
2018-02-11 22:27:15 -0500

    .

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from img2html.converter import Img2HTMLConverter
 import os
 
-converter = Img2HTMLConverter(font_size=10, background='black')
-html = converter.convert(os.getcwd() + '/pic.png')
+converter = Img2HTMLConverter(font_size=4, background='black')
+html = converter.convert(os.getcwd() + '/gta.jpg')
 with open(os.getcwd() + '/a.html', mode='w') as f:
     f.write(str(html))
